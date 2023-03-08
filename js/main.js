a0f50e474e5c76ecb9afc4db29a2159a75827ab5
Showman_Das <Showmandas3216@gmail.com>
2023-03-09 00:02:42 +0600

    show meal  cards on site

--- js/main.js
+++ js/main.js
@@ -2,7 +2,30 @@ const loadMeals=(searchValue)=>{
     const url=`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`;
     fetch(url)
     .then(res=>res.json())
-    .then(data=>console.log(data.meals))
+    .then(data=>showMealCards(data.meals.slice(0,3)))
+}
+
+const showMealCards= meals => {
+    console.log(meals);
+    const cardSection=document.getElementById('card-section ');
+    cardSection.innerHTML=''; 
+    meals.forEach(meal => {
+        // console.log(meal);
+        const cardDiv=document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML=`
+        <div class="card">
+      <img src="${meal.strMealThumb}" class="img-fluid w-100" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0,100)}</p>
+      </div>
+    </div>
+        `
+        cardSection.appendChild(cardDiv);
+    })
+    
+
 }
 
 document.getElementById('searchBtn').addEventListener('click',()=>{
@@ -11,4 +34,4 @@ document.getElementById('searchBtn').addEventListener('click',()=>{
     loadMeals(searchValue);
 })
 
-loadMeals();
+loadMeals('egg');
